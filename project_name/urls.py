8d0b8a83f339cd60b520348f953d6112549a32ae
Hamim Al Mahdi Russell <russell310@gmail.com>
2018-02-03 03:21:19 +0600

    urls prettify

--- project_name/urls.py
+++ project_name/urls.py
@@ -18,41 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from accounts import views as accounts_views
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.decorators import login_required
-
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',login_required(TemplateView.as_view(template_name="home.html")), name = 'home'),
+    path('',TemplateView.as_view(template_name="home.html"), name = 'home'),
+    path('account/', include('accounts.urls')),
     
-
-    # ACCOUNT MANAGEMENT
-    path('signup/', accounts_views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
-    path('reset/', auth_views.PasswordResetView.as_view(
-            template_name='password_reset.html',
-            email_template_name='password_reset_email.html',
-            subject_template_name='password_reset_subject.txt'
-        ), name='password_reset'),
-    path('reset/done/', auth_views.PasswordResetDoneView.as_view(
-            template_name='password_reset_done.html'
-        ), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-            template_name='password_reset_confirm.html'
-        ), name='password_reset_confirm'),
-    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
-            template_name='password_reset_complete.html'
-        ), name='password_reset_complete'),
-    path('oauth/', include('social_django.urls', namespace='social')), 
 ]
 
 
