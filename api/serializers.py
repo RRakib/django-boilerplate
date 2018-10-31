cb3bcc7f3219483d494d51becd3be04b64f1085a
Hamim Al Mahdi Russell <russell310@gmail.com>
2018-11-01 00:33:42 +0600

    add datatables

--- api/serializers.py
+++ api/serializers.py
@@ -3,8 +3,13 @@ from .models import DemoPurpose
 
 
 class DemoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DemoPurpose
-        fields = ('url', 'id', 'name')
+	id = serializers.IntegerField(read_only=True)
+	class Meta:
+		model = DemoPurpose
+		fields = ('url', 'id', 'name')
 
+		
+		# Specifying fields in datatables_always_serialize
+        # will also force them to always be serialized.
+		datatables_always_serialize = ('id',)
 
--- project_name/settings.py
+++ project_name/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_datatables',
 
     # myapp
     'accounts',
@@ -171,7 +172,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
-    # 'DEFAULT_RENDERER_CLASSES': (
-    # 'rest_framework.renderers.JSONRenderer',
-    # )
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_datatables.renderers.DatatablesRenderer',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_datatables.filters.DatatablesFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
+    'PAGE_SIZE': 50,
 }
