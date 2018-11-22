33c56248563d4af78fd7eca85e808f5ee25af09d
russell310 <russell310@gmail.com>
2018-11-23 02:56:43 +0600

    Update views.py

--- api/views.py
+++ api/views.py
@@ -6,3 +6,13 @@ from .serializers import DemoSerializer
 class DemoViewSet(viewsets.ModelViewSet):
     queryset = DemoPurpose.objects.all()
     serializer_class = DemoSerializer
+
+    def get_serializer(self, *args, **kwargs):
+	    if "data" in kwargs:
+	        data = kwargs["data"]
+
+	        # check if many is required
+	        if isinstance(data, list):
+	            kwargs["many"] = True
+
+	    return super(DemoViewSet, self).get_serializer(*args, **kwargs)
