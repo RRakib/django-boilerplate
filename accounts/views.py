081238b703fdd2e5ee42fee8002c75a673e6c198
russell310 <russell310@gmail.com>
2019-10-16 13:12:22 +0600

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import login as auth_login
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 from .forms import SignUpForm
@@ -8,6 +8,7 @@ from .forms import SignUpForm
 # Create your views here.
 
 class SignUpView(CreateView):
+    model = User
     form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
