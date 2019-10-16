2927f2fb1f1e2ad96c486543466e9c5fa882621d
russell310 <russell310@gmail.com>
2019-10-16 13:00:39 +0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views as accounts_views
 
 urlpatterns = [
     # ACCOUNT MANAGEMENT
-    path('signup/', accounts_views.signup, name='signup'),
+    path('signup/', accounts_views.SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
